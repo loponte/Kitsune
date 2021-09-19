a390ec5b900db80c78303db160c4127544d0be0d
Lucas Feijo <lucasfeijo98@gmail.com>
2021-09-18 22:01:47 -0300

    Update

--- index.js
+++ index.js
@@ -88,20 +88,6 @@ client.on("message", async message => {
   }
 })
 
-client.on('voiceStateUpdate', (oldState, newState) => {
-
-  // if nobody left the channel in question, return.
-  if (oldState.channelID !== oldState.guild.me.voice.channelID || newState.channel)
-    return;
-
-  // otherwise, check how many people are in the channel now
-  if (!(oldState.channel.members.size - 1))
-    setTimeout(() => { // if 1 (you), wait five minutes
-      if (!(oldState.channel.members.size - 1)) // if there's still 1 member, 
-        oldState.channel.leave(); // leave
-    }, 60000); // (1 min in ms)
-});
-
 const status = queue => `Volume: ${queue.volume}% | Filtro: ${queue.filter || "Off"} | Loop: ${queue.repeatMode ? queue.repeatMode === 2 ? "Playlist total" : "Musica" : "Off"} | Autoplay: ${queue.autoplay ? "On" : "Off"}`
 client.distube
   .on("playSong", async (message, queue, song) => {
